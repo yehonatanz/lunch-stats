16f0e5a99212176b03f9f4d67fdf96e7378a32b0
Yehonatan Zecharia <yonti95@gmail.com>
2022-05-01 23:46:12 +0300

    Prettier index.js

--- docs/index.js
+++ docs/index.js
@@ -8,11 +8,13 @@ function uniq(arr) {
 }
 
 function search(patterns) {
-  const traces = patterns.map(pattern =>
+  const traces = patterns.map((pattern) =>
     reportsToTrace(
-      state.allReports.filter(report => report.resturaunts.toLowerCase().includes(pattern.toLowerCase())),
-      pattern,
-    ),
+      state.allReports.filter((report) =>
+        report.resturaunts.toLowerCase().includes(pattern.toLowerCase())
+      ),
+      pattern
+    )
   );
   Plotly.react(CHART, traces, LAYOUT, CONFIG);
 }
@@ -32,33 +34,39 @@ function reportsToTrace(reports, name) {
     type: 'scatter',
     mode: 'markers',
     name: name || 'הכל',
-    text: reports.map(report => report.resturaunts),
-    x: reports.map(report => report.date),
-    y: reports.map(report => '1970-01-01 ' + report.time),
+    text: reports.map((report) => report.resturaunts),
+    x: reports.map((report) => report.date),
+    y: reports.map((report) => '1970-01-01 ' + report.time),
   };
 }
 const CHART = document.getElementById('chart');
 const LAYOUT = {
   title: 'מתי דיווחו על הגעה?',
   showlegend: true,
-  yaxis: { range: ['1970-01-01 11:00:00', '1970-01-01 15:00:00'], tickformat: '%H:%M' },
+  yaxis: {
+    range: ['1970-01-01 11:00:00', '1970-01-01 15:00:00'],
+    tickformat: '%H:%M',
+  },
 };
 const CONFIG = { scrollZoom: true };
 
 const state = {};
-fetch('./build/reports.json').then(response => response.json()).then(reports => {
-  state.allReports = reports;
-  createDropDown(
-    uniq(
-      state.allReports.map(report => report.resturaunts.split(/\s+/))
-        .flat()
-        .map(word => word.trim()),
-    ),
-  );
-  $('.js-example-basic-multiple').select2({
-    createTag: tag => ({ id: tag.term, text: tag.term, tag: true }),
-    multiple: true,
-    tags: true,
+fetch('./build/reports.json')
+  .then((response) => response.json())
+  .then((reports) => {
+    state.allReports = reports;
+    createDropDown(
+      uniq(
+        state.allReports
+          .map((report) => report.resturaunts.split(/\s+/))
+          .flat()
+          .map((word) => word.trim())
+      )
+    );
+    $('.js-example-basic-multiple').select2({
+      createTag: (tag) => ({ id: tag.term, text: tag.term, tag: true }),
+      multiple: true,
+      tags: true,
+    });
+    Plotly.newPlot(CHART, [reportsToTrace(state.allReports)], LAYOUT, CONFIG);
   });
-  Plotly.newPlot(CHART, [reportsToTrace(state.allReports)], LAYOUT, CONFIG);
-});
