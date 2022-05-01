f89f57e4e3b3c2a7e4ff53a74a5fdb9a17cdda53
Yehonatan Zecharia <yonti95@gmail.com>
2022-05-02 00:04:02 +0300

    Split search results from trace construction

--- docs/index.js
+++ docs/index.js
@@ -1,43 +1,37 @@
 $('#restaurant-dropdown').on('select2:close', function () {
   const selectedValues = $('#restaurant-dropdown').select2('data');
-  search(selectedValues.map(({ text }) => text));
+  onSearch(selectedValues.map(({ text }) => text));
 });
 
 function uniq(arr) {
   return [...new Set(arr)].sort();
 }
 
-function search(patterns) {
-  const traces = patterns.map((pattern) =>
-    reportsToTrace(
-      state.allReports.filter((report) =>
-        report.resturaunts.toLowerCase().includes(pattern.toLowerCase())
-      ),
-      pattern
-    )
-  );
-  Plotly.react(CHART, traces, LAYOUT, CONFIG);
+const state = {};
+function search(reports, patterns) {
+  return patterns.map((pattern) => ({
+    pattern,
+    reports: reports.filter((report) =>
+      report.resturaunts.toLowerCase().includes(pattern.toLowerCase())
+    ),
+  }));
 }
 
-function createDropDown(values) {
-  const dropdown = document.getElementById('restaurant-dropdown');
-  for (let value of values) {
-    const opt = document.createElement('option');
-    opt.value = value;
-    opt.innerText = value;
-    dropdown.appendChild(opt);
-  }
+function onSearch(patterns) {
+  const searchResults = search(state.allReports, patterns);
+  updateChart(searchResults);
 }
 
-function reportsToTrace(reports, name) {
-  return {
-    type: 'scatter',
-    mode: 'markers',
-    name: name || 'הכל',
-    text: reports.map((report) => report.resturaunts),
-    x: reports.map((report) => report.date),
-    y: reports.map((report) => '1970-01-01 ' + report.time),
-  };
+const resultToTrace = ({ pattern, reports }) => ({
+  type: 'scatter',
+  mode: 'markers',
+  name: pattern || 'הכל',
+  text: reports.map((report) => report.resturaunts),
+  x: reports.map((report) => report.date),
+  y: reports.map((report) => '1970-01-01 ' + report.time),
+});
+function updateChart(searchResults) {
+  Plotly.react(CHART, searchResults.map(resultToTrace), LAYOUT, CONFIG);
 }
 const CHART = document.getElementById('chart');
 const LAYOUT = {
@@ -50,7 +44,16 @@ const LAYOUT = {
 };
 const CONFIG = { scrollZoom: true };
 
-const state = {};
+function createDropDown(values) {
+  const dropdown = document.getElementById('restaurant-dropdown');
+  for (let value of values) {
+    const opt = document.createElement('option');
+    opt.value = value;
+    opt.innerText = value;
+    dropdown.appendChild(opt);
+  }
+}
+
 fetch('./build/reports.json')
   .then((response) => response.json())
   .then((reports) => {
@@ -68,5 +71,11 @@ fetch('./build/reports.json')
       multiple: true,
       tags: true,
     });
-    Plotly.newPlot(CHART, [reportsToTrace(state.allReports)], LAYOUT, CONFIG);
+    Plotly.newPlot(
+      CHART,
+      [resultToTrace({ reports: state.allReports })],
+      LAYOUT,
+      CONFIG
+    );
+    onSearch(['']);
   });
